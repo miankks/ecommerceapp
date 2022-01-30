27f9f7f2d2c91b0bfaec6d573d44bda6ec9b2a2f
Mian King <Bilal Jan>
2022-01-30 16:18:33 +0100

    first commit

---
+++ ecommerce/src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://cdn.mos.cms.futurecdn.net/dHCvBmiStozFgvvNzJemWk-1280-80.jpg.webp",
+    title: "Summer sale",
+    desc: "Don/'t compromise on style! get flat 30% off for new arrivals.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://fashionbagsplaza.com/wp-content/uploads/2021/05/photo-1483985988355-763728e1935b-5.jpg",
+    title: "Winter sale",
+    desc: "Don/'t compromise on style! get flat 30% off for new arrivals.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://stylecaster.com/wp-content/uploads/2021/04/2021-loungewear-trends.jpg",
+    title: "Loungewear Love",
+    desc: "Don/'t compromise on style! get flat 30% off for new arrivals.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://cdn.mos.cms.futurecdn.net/dHCvBmiStozFgvvNzJemWk-1280-80.jpg.webp",
+    title: "Short style",
+  },
+  {
+    id: 2,
+    img: "https://fashionbagsplaza.com/wp-content/uploads/2021/05/photo-1483985988355-763728e1935b-5.jpg",
+    title: "Loungewear Love",
+  },
+  {
+    id: 3,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+    title: "Light jackets",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 2,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 3,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 4,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 5,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 6,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 7,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+  {
+    id: 8,
+    img: "https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg",
+  },
+];
---
+++ ecommerce/src/pages/Cart.js
@@ -0,0 +1,249 @@
+import { Add, Remove } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  ${mobile({ padding: "10px" })}
+`;
+
+const Title = styled.h1`
+font-weight 300; 
+text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopText = styled.div`
+  ${mobile({ display: "none" })}
+`;
+
+const TopTexts = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin 0px 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${mobile({ flexDirection: "column" })}
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+    width: 20px;
+    height 20px;
+    border-radius 50%;
+    background-color: ${(props) => props.color}
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+  ${mobile({ margin: "5px 15px" })}
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+  ${mobile({ marginBottom: "20px" })}
+`;
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgrey;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Hr = styled.hr`
+  background-color: #eee;
+  border: none;
+  height: 1px;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>Your bag</Title>
+        <Top>
+          <TopButton>Continue shopping</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag (2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">Checkout now</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://png.pngtree.com/png-clipart/20190604/original/pngtree-restaurant-waiter-male-suit-png-image_1267605.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Jessie Thunder
+                  </ProductName>
+                  <ProductId>
+                    <b>ProductID: </b>3487343233
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size: </b>37.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <Hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://fashionbagsplaza.com/wp-content/uploads/2021/05/photo-1483985988355-763728e1935b-5.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b>Hakura Wear
+                  </ProductName>
+                  <ProductId>
+                    <b>ProductID: </b>3487343233
+                  </ProductId>
+                  <ProductColor color="maroon" />
+                  <ProductSize>
+                    <b>Size: </b>M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$ 30</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>Order Summary</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping discount</SummaryItemText>
+              <SummaryItemPrice>$ -5</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>Checkout Now</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
